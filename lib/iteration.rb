8ab3dc83e2dd8da931f9efd55938155d17d0c827
nickie kane <kameo712@gmail.com>
2020-01-02 17:14:56 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -52,7 +52,7 @@ def total_even_pairs(src)
   while row_index < src.length do 
      element_index = 0
      if (src[row_index][element_index] % 2 == 0) && (src[row_index][element_index + 1] % 2 == 0)
-       binding.pry
+       
         total += (src[row_index][element_index]) + (src[row_index][element_index + 1])
      end
      row_index += 1 
